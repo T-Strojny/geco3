1840611f0e808ea06017baa7884849f72f58458f
Tymoteusz Strojny <117780025+T-Strojny@users.noreply.github.com>
2024-09-03 14:55:53 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="geco-data-generator-corruptor-p3",
+    version="0.1.0",
+    author="Tymoteusz Strojny",
+    author_email="tymek.strojny@gmail.com",
+    description="A flexible data generation and corruption system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/T-Strojny/Python3-GeCo-data-generator-corurptor",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        # Add any dependencies here
+    ],
+    package_data={
+        "geco_data_generator_corruptor-p3": ["lookup-files/*.csv"],
+    },
+)
