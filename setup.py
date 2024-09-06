96de988da992525d0fdb49186f1f2604095d3f90
Tymoteusz Strojny <tymek.strojny@gmail.com>
2024-09-06 15:57:03 +0200

    Updated project structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Python3_GeCo_data_generator_corurptor",
+    version="v0.1.0-alpha",
+    author="Tymoteusz Strojny",
+    author_email="tymek.strojny@gmail.com",
+    description="A flexible data generation and corruption system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/T-Strojny/Python3_GeCo_data_generator_corurptor",
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+)
