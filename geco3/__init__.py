c40f5c3359a2fb077c558711dbdecd54b5b59c3f
Tymoteusz Strojny <tymek.strojny@gmail.com>
2024-09-06 20:57:01 +0200

    commit

---
+++ geco3/__init__.py
@@ -0,0 +1,13 @@
+from . import basefunctions
+from . import attrgenfunct
+from . import contdepfunct
+from . import corruptor
+from . import generator
+
+__all__ = [
+    "basefunctions",
+    "attrgenfunct",
+    "contdepfunct",
+    "corruptor",
+    "generator",
+]
